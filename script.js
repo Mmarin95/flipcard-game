050ff18de76f62039055dc77885cc71f464d1f03
Miquel Marin <mmarin_79@hotmail.com>
2021-02-14 20:43:54 +0100

    add CleanBefore roundfunction

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 import Deck from "./Deck.js";
-const computerCardSlot = document.querySelector(".computer-card-slot")
+const computerCardSlot = document.querySelector(".computer-card-slot");
+const playerCardSlot = document.querySelector(".player-card-slot");
+const computerDeckElement = document.querySelector(".computer-deck");
+const playerDeckElement = document.querySelector(".player-deck");
+const textElement = document.querySelector(".text");
+let playerDeck, computerDeck;
 
-const deck = new Deck();
-deck.shuffle();
+startGame();
 
-computerCardSlot.appendChild(deck.cards[0].getHTML())
+function startGame() {
+  const deck = new Deck();
+  deck.shuffle();
+
+  const deckMidpoint = Math.ceil(deck.numCards / 2);
+  playerDeck = new Deck(deck.cards.slice(0, deckMidpoint));
+  computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numCards));
+
+  cleanBeforeRound();
+}
+
+function cleanBeforeRound() {
+  computerCardSlot.innerHTML = "";
+  playerCardSlot.innerHTML = "";
+  textElement.innerText = "";
+  updateDeckCont()
+}
+
+function updateDeckCont() {
+  computerDeckElement.innerText = computerDeck.numCards
+  playerDeckElement.innerText = playerDeck.numCards
+}
