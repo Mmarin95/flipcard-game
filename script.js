cbb6f3fd606e914e98ab319e43cf3b902f98cc03
Miquel Marin <mmarin_79@hotmail.com>
2021-02-14 20:29:27 +0100

    fix css classes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import Deck from "./Deck.js";
+const computerCardSlot = document.querySelector(".computer-card-slot")
 
 const deck = new Deck();
 deck.shuffle();
 
-
+computerCardSlot.appendChild(deck.cards[0].getHTML())
