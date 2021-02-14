ed095a3cceaf93d30f92e6d588ad364074e00897
Miquel Marin <mmarin_79@hotmail.com>
2021-02-14 21:07:28 +0100

    add game functionality

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 import Deck from "./Deck.js";
+
+const CARD_VALUE_MAP = {
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10,
+  J: 11,
+  Q: 12,
+  K: 13,
+  A: 14,
+};
+
 const computerCardSlot = document.querySelector(".computer-card-slot");
 const playerCardSlot = document.querySelector(".player-card-slot");
 const computerDeckElement = document.querySelector(".computer-deck");
 const playerDeckElement = document.querySelector(".player-deck");
 const textElement = document.querySelector(".text");
-let playerDeck, computerDeck;
+let playerDeck, computerDeck, inRound, stop;
+
+document.addEventListener("click", () => {
+  if (stop) {
+    startGame();
+    return;
+  }
+
+  if (inRound) {
+    cleanBeforeRound();
+  } else {
+    flipCards();
+  }
+});
 
 startGame();
 
@@ -15,18 +45,61 @@ function startGame() {
   const deckMidpoint = Math.ceil(deck.numCards / 2);
   playerDeck = new Deck(deck.cards.slice(0, deckMidpoint));
   computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numCards));
-
+  inRound = false;
   cleanBeforeRound();
 }
 
 function cleanBeforeRound() {
+  inRound = false;
   computerCardSlot.innerHTML = "";
   playerCardSlot.innerHTML = "";
   textElement.innerText = "";
-  updateDeckCont()
+  updateDeckCont();
 }
 
 function updateDeckCont() {
-  computerDeckElement.innerText = computerDeck.numCards
-  playerDeckElement.innerText = playerDeck.numCards
+  computerDeckElement.innerText = computerDeck.numCards;
+  playerDeckElement.innerText = playerDeck.numCards;
+}
+
+function flipCards() {
+  inRound = true;
+
+  const playerCard = playerDeck.pop();
+  const computerCard = computerDeck.pop();
+
+  playerCardSlot.appendChild(playerCard.getHTML());
+  computerCardSlot.appendChild(computerCard.getHTML());
+
+  updateDeckCont();
+
+  if (isRoundWinner(playerCard, computerCard)) {
+    textElement.innerText = "Win";
+    playerDeck.push(playerCard);
+    playerDeck.push(computerCard);
+  } else if (isRoundWinner(computerCard, playerCard)) {
+    textElement.innerText = "Lose";
+    computerDeck.push(playerCard);
+    computerDeck.push(computerCard);
+  } else {
+    textElement.innerText = "Draw";
+    playerDeck.push(playerCard);
+    computerDeck.push(computerCard);
+  }
+
+  if (isGameOver(playerDeck)) {
+    textElement.innerText = "You Lose!!";
+    stop = true;
+  } else if (isGameOver(computerDeck)) {
+    textElement.innerText = "You Win!!";
+    stop = true;
+  }
+}
+
+function isRoundWinner(cardOne, cardTwo) {
+  return CARD_VALUE_MAP[cardOne.card_face] > CARD_VALUE_MAP[cardTwo.card_face];
+}
+
+function isGameOver(deck) {
+  return deck.numCards === 0;
 }
